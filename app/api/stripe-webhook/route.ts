1025b49fc6238da22352ca63fb1a7f4b64ab23b4
dermeistereder <office@meister-eder.at>
2026-04-02 14:26:14 +0200

    Update route.ts

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -19,7 +19,7 @@ export async function POST(req: NextRequest) {
 
   switch (event.type) {
     case 'checkout.session.completed': {
-      const session = event.data.object as Stripe.CheckoutSession
+      const session = event.data.object as Stripe.Checkout.Session
       const { listing_id } = session.metadata || {}
       if (listing_id) {
         await supabase.from('listings').update({
